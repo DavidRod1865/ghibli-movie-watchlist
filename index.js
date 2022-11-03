cc1a55844ac411729b8e13efe01f1541ffe98bb3
David Rodriguez <David.Rod1865@gmail.com>
2022-11-02 20:24:26 -0400

    updated api

--- index.js
+++ index.js
@@ -1,43 +1,55 @@
-const express = require('express') //able to use express
+//able to use express
+const express = require('express') 
 const app = express()
-const bodyParser = require('body-parser') //let's you use request.body
+
+//let's you use request.body
+const bodyParser = require('body-parser') 
 const path = require('path')
 const { callbackify } = require('util')
 
+// Database Route
 const MongoClient = require('mongodb').MongoClient
 const uri = "mongodb+srv://davidrod1865:David123@ghibli2.lvxc6di.mongodb.net/?retryWrites=true&w=majority"
 const client = new MongoClient(uri)
 
-// parse application/x-www-form-rsurlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
-// parse application/json
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
-// set the view engine to ejs
+
+// Use View Engine
 app.set('view engine', 'ejs');
 
-// // Connect to the database & connection
+// // Connect to the Database & Render Page
 client.connect()
     .then((client) => {
         const ghibliMovies = client.db("ghibli2")
         const watchList = ghibliMovies.collection("watch-list")
         
         //creates index page on the local host
-        app.get('/', (req, res) => {
-            // fetchMovies(movies => {
-            //     let movieDetails = {
-            //         title: movies.title,
-            //         movieID: movies.id,
-            //         description: movies.description,
-            //         movieImage: movies.image
-            //     }
-            //     console.log(movies)
-                res.render('pages/index.ejs')
-            })
-        console.log("Database 3000!")
-
-        app.post('/addMovie', async (req, res) => {
-
+        app.get('/', async (req, res) => {
+            fetch(`https://ghibliapi.herokuapp.com/films`)
+                .then(res => {
+                    if (res.ok){
+                        let data = res.json()
+                        return data
+                        } else {
+                            console.log("API Not Connected")
+                            }
+                    })
+                .then(data => {
+                    Object.keys(data).forEach( key => {
+                        console.log(`${key} : ${data[key]}`);
+                        });
+                    console.log(data[0].id)
+                    console.log(data[0].title)
+                    console.log(data[0].director)
+                })
+                .catch(err => { 
+                    console.log('API Error') 
+                })
+            res.render('pages/index.ejs')
         })
+    console.log("Database 3000!")
     })
     .catch((err) => console.error('Database not connected!'))
 
@@ -45,27 +57,3 @@ client.connect()
 app.listen(3001, () => {
     console.log(`Ghibli in this Bitch!`);
 });
-
-async function fetchMovies(){
-    let movies = await fetch(`https://ghibliapi.herokuapp.com/films`)
-    console.log(movies.status); // 200
-    console.log(movies.statusText); // OK
-    
-    if (movies.status === 200){
-        let movieData = await movies.image
-        console.log(movieData)
-    }
-    // .then(res => {
-    //         if (res.ok) {
-    //             return res.json()
-    //             } else {
-    //                 throw "No Path to API";
-    //                 }
-    //         })
-    //     .then(res => {
-    //         return movies(res);
-    //     })
-    //     .catch(err => {console.log('API Error')})
-}
-
-fetchMovies()
